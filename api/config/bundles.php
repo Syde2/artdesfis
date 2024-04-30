2ae84c4bcb5e3f7799e4444c07ce9e9a6bf223b5
Syde2 <contact@syde2hub.com>
2024-04-30 15:53:56 +0200

    back

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -10,4 +10,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
