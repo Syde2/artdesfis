98a8daf09132a75944da5f2ed255f7f6ed1021b1
Syde2 <contact@syde2hub.com>
2024-07-28 20:51:43 +0200

    Ajustements envoi SMS

--- api/src/EventListener/ContactCreationListener.php
+++ api/src/EventListener/ContactCreationListener.php
@@ -6,16 +6,27 @@ use App\Entity\Contact;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Notifier\Message\SmsMessage;
+use Symfony\Component\Notifier\TexterInterface;
 
 class ContactCreationListener
 {
     private $mailer;
+    private $texter;
     private $toEmail;
+    private $toPhone;
 
-    public function __construct(MailerInterface $mailer, string $toEmail)
-    {
+    public function __construct(
+        MailerInterface $mailer, 
+        TexterInterface $texter, 
+        string $toEmail, 
+        ?string $toPhone = null
+    ) {
         $this->mailer = $mailer;
+        $this->texter = $texter;
         $this->toEmail = $toEmail;
+        $this->toPhone = $toPhone;
+
     }
 
     public function postPersist(LifecycleEventArgs $args): void
@@ -25,6 +36,7 @@ class ContactCreationListener
             return;
         }
 
+        // Send email
         $email = (new Email())
             ->from($this->toEmail)
             ->to($this->toEmail)
@@ -32,5 +44,20 @@ class ContactCreationListener
             ->text($contact->getMessage());
 
         $this->mailer->send($email);
+
+        // Send SMS if phone number is set
+        $sms = new SmsMessage(
+            // the phone number to send the SMS message to
+            $this->toPhone,
+            // the message
+            "Vous avez un nouveau message de la part de l'art des fils : \n".$contact->getMessage(),
+            // optionally, you can override default "from" defined in transports
+            // '+1422222222',
+            // you can also add options object implementing MessageOptionsInterface
+            // $options
+        );
+
+            $this->texter->send($sms);
+        
     }
 }
