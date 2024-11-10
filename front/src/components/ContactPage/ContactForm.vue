e8e62f5e46cc0f31bef8a7f577378fa0906fb0db
Syde2 <contact@syde2hub.com>
2024-11-10 15:48:50 +0100

    Ajout retour sur la page acceuil apres Submit Contact

--- front/src/components/ContactPage/ContactForm.vue
+++ front/src/components/ContactPage/ContactForm.vue
@@ -2,6 +2,7 @@
 import { useQuasar } from 'quasar'
 import { ref, watch } from 'vue'
 import { api } from 'src/boot/axios';
+import { useRouter } from 'vue-router';
 
 const props = defineProps({
     produit: Object
@@ -14,6 +15,7 @@ const article = ref('')
 const reference = ref('')
 const commentaire = ref('')
 const accept = ref(false)
+const router = useRouter()
 
 watch(() => props.produit, (newProduit) => {
     if (newProduit) {
@@ -36,17 +38,16 @@ function onSubmit() {
     }
 }
 
-
-async function PostForm(){
+async function PostForm() {
     const url = ('/contacts')
     const payload = {
         "email": email.value,
         "message": commentaire.value,
     }
-    if(article.value){
+    if (article.value) {
         payload.nomArticle = article.value
     }
-    if(reference.value){
+    if (reference.value) {
         payload.referenceArticle = reference.value
     }
     try {
@@ -56,17 +57,18 @@ async function PostForm(){
             message: 'Formulaire envoyé avec succès!',
             icon: 'check'
         });
+        router.push('/catalogue')
         return response.data;
     }
 
-    catch (error){
+    catch (error) {
         console.error('Erreur lors de l\'envoi du formulaire:', error);
         $q.notify({
             color: 'negative',
             message: 'Erreur lors de l\'envoi du formulaire. Veuillez réessayer.',
             icon: 'error'
-      });
-      throw error;
+        });
+        throw error;
     }
 
 }
@@ -76,29 +78,30 @@ function onReset() {
     accept.value = false
     commentaire.value = null
 }
+
 </script>
 
 <template>
-<q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md q-pa-md">
-    
-    <div v-if="article" class="row q-px-md" >
-        <q-input class="col-8" borderless type="text" v-model="article" label="Produit souhaité" readonly />
-        <q-input class="col-4" borderless type="text" v-model="reference" label="Reference" readonly />
-    </div>
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md q-pa-md">
 
-        <q-input  rounded outlined type="email" v-model="email" label="Adresse mail*"
-                hint="*Adresse à laquelle je peux être recontactée" lazy-rules
-                :rules="[val => val && val.length > 0 || 'Indiquez votre adresse mail']" />
+        <div v-if="article" class="row q-px-md">
+            <q-input class="col-8" borderless type="text" v-model="article" label="Produit souhaité" readonly />
+            <q-input class="col-4" borderless type="text" v-model="reference" label="Reference" readonly />
+        </div>
 
+        <q-input rounded outlined type="email" v-model="email" label="Adresse mail*"
+            hint="*Adresse à laquelle je peux être recontactée" lazy-rules
+            :rules="[val => val && val.length > 0 || 'Indiquez votre adresse mail']" />
 
-    <q-input class="" v-model="commentaire" rounded outlined type="textarea" label="Commentaire" />
+        <q-input class="" v-model="commentaire" rounded outlined type="textarea" label="Commentaire" />
 
-    <q-toggle v-model="accept" class="text-caption " color="accent"
-        label=" J'accepte que les informations saisies dans ce formulaire soient utilisées pour permettre de me recontacter" />
+        <q-toggle v-model="accept" class="text-caption " color="accent"
+            label=" J'accepte que les informations saisies dans ce formulaire soient utilisées pour permettre de me recontacter" />
 
-    <div>
-        <q-btn label="Envoyer" type="submit" color="accent" />
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-    </div>
-</q-form>
+        <div>
+            <q-btn label="Envoyer" type="submit" color="accent" />
+            <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+
+    </q-form>
 </template>
